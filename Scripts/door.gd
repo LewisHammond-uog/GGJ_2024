a6324120edd4ca0137bcbb757688fa9485c4c239
Doglodyte <benwelljoshua@gmail.com>
2024-01-27 16:18:01 +0000

    Doors now colour code

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,5 +1,21 @@
 extends Node3D
 
+var mat_black : Material = preload("res://Enviroment/Objects/Materials/strips/strip_black.tres")
+var mat_green : Material = preload("res://Enviroment/Objects/Materials/strips/strip_green.tres")
+var mat_blue : Material = preload("res://Enviroment/Objects/Materials/strips/strip_blue.tres")
+var mat_red : Material = preload("res://Enviroment/Objects/Materials/strips/strip_red.tres")
+var mat_purple : Material = preload("res://Enviroment/Objects/Materials/strips/strip_purple.tres")
+var mat_gold : Material = preload("res://Enviroment/Objects/Materials/strips/strip_gold.tres")
+
+var strip_mats = [
+	mat_black,
+	mat_green,
+	mat_red, 
+	mat_blue,
+	mat_purple,
+	mat_gold
+]
+
 var opening : bool = false
 var want_open : bool = false
 
@@ -12,6 +28,9 @@ var lower_amount = raise_amount * -1
 @export var security_level : Globals.SecurityLevel
 
 var raised : bool = false
+	
+func _ready():
+	_assign_strip_colour()
 	
 func _door_toggle():
 	_security_check()
@@ -58,3 +77,9 @@ func _on_area_3d_body_exited(body):
 	raised = false
 	want_open = false
 	_door_toggle()
+
+func _assign_strip_colour():
+	var chosen_mat : Material
+	chosen_mat = strip_mats[security_level]
+	$"Colour Strip".set_surface_override_material(0, chosen_mat)
+	pass
