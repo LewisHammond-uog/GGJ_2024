e388dbfa3eec7ed47bb14c9fc04c1588cde82c29
Lewis Hammond <bubblemapgaming@gmail.com>
2024-01-27 19:40:59 +0000

    stuff and things

---
+++ Scripts/HealthDisplay.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+using System.Globalization;
+using GGJ24;
+using GGJ24.Scripts;
+
+public partial class HealthDisplay : Control
+{
+	[Export] private RichTextLabel Label;
+	[Export] private PlayerHealth Player;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Player = GetNode("/root/TestScene/Player").FindChild("PlayerHealth") as PlayerHealth;
+	}
+	
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Player == null)	
+		{
+			return;
+		}
+
+		Label.Text = $"[center]{Mathf.CeilToInt(Player.CurrentHealth).ToString(CultureInfo.InvariantCulture)}[/center]";
+	}
+	
+}
