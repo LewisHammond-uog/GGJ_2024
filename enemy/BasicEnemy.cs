0a642c0fc38cdb7095d4d2c8452665d9c7a10ef6
Lewis Hammond <bubblemapgaming@gmail.com>
2024-01-27 13:07:41 +0000

    Squashed commit of the following:
    commit 69c3440818a7ced97ab1f382389cfc239a4c1ba6
    Author: Lewis Hammond <bubblemapgaming@gmail.com>
    Date:   Sat Jan 27 13:06:32 2024 +0000
    
        ability to shoot
    
    commit b595496ff38104c71b5dce1d1f7de4feec5ea81f
    Author: Lewis Hammond <bubblemapgaming@gmail.com>
    Date:   Sat Jan 27 12:21:36 2024 +0000
    
        dont move if cant see player
    
    commit a1679247c8f7ef1f09ac674bb50c76b3e70c6025
    Author: Lewis Hammond <bubblemapgaming@gmail.com>
    Date:   Sat Jan 27 12:14:52 2024 +0000
    
        Basic Move AI
    
    commit 61c591a367094ee04477db2114824776618dcbbd
    Author: Lewis Hammond <bubblemapgaming@gmail.com>
    Date:   Sat Jan 27 10:50:04 2024 +0000
    
        canseeplayer
    
    commit ff4b68f771bbbf568c0ec2106eb80c4cd2911ac5
    Author: Lewis Hammond <bubblemapgaming@gmail.com>
    Date:   Fri Jan 26 22:07:57 2024 +0000
    
        Empty enemy object

---
+++ enemy/BasicEnemy.cs
@@ -0,0 +1,163 @@
+using System;
+using Godot;
+using Godot.Collections;
+
+namespace GGJ24.enemy;
+
+public partial class BasicEnemy : CharacterBody3D
+{
+	[Export] private NavigationAgent3D agent;
+	[Export] private EnemyShooter shootComp;
+	[Export] private float seeDistance = 1000f;
+	[Export] private float idealDistanceToPlayer = 1f;
+	[Export] private float standStillDistance = 0.2f;
+	[Export] private float moveSpeed = 2f;
+	[Export] private CharacterBody3D player;
+
+	private bool canSeePlayer = false;
+	private Vector3? playerLastPos;
+	private Vector3 movePos;
+	private State state;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		state = State.Idle;
+		player = GetNode<CharacterBody3D>("/root/TestScene/Player");
+		movePos = Position;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		UpdateState();
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		canSeePlayer = CanSeePlayer();
+		calculateMove(delta);
+	}
+
+	private void calculateMove(double dt)
+	{
+		agent.TargetPosition = movePos;
+		Vector3 nextPos = agent.GetNextPathPosition();
+		Vector3 vel = Position.DirectionTo(nextPos) * moveSpeed;
+		agent.Velocity = vel;
+	}
+
+	private bool CanSeePlayer()
+	{
+		if (player == null)
+		{
+			return false;
+		}
+		
+		var spaceState = GetWorld3D().DirectSpaceState;
+		var query = PhysicsRayQueryParameters3D.Create(Position, player.Position);
+		query.Exclude = new Array<Rid>(new Rid[] { GetRid(), player.GetRid() });
+		var result = spaceState.IntersectRay(query);
+
+		if (result.Count == 0)
+		{
+			playerLastPos = player.Position;
+		}
+		
+		return result.Count == 0;
+	}
+
+	//THIS IS A MESS
+	private void UpdateState()
+	{
+		switch (state)
+		{
+			case State.Idle:
+				if (canSeePlayer)
+				{
+					state = State.MoveToPlayer;
+					break;
+				}
+				if (playerLastPos != null)
+				{
+					movePos = playerLastPos.Value;
+				}
+				break;
+			case State.MoveToPlayer:
+			{
+				//If we are close enough to shoot and can shoot the player
+				if (shootComp is { CanShoot: true } && Position.DistanceTo(player.Position) < shootComp.MaxShootDist)
+				{
+					state = State.MoveAndShoot;
+				}
+
+				Vector2 intercept = GetPlayerRadiusInterceptXZ();
+				movePos = new Vector3(intercept.X, player.Position.Y, intercept.Y);
+			}
+				break;
+			case State.MoveAndShoot:
+			{
+				if (shootComp is { CanShoot: false } || Position.DistanceTo(player.Position) > shootComp.MaxShootDist)
+				{
+					state = State.MoveToPlayer;
+					break;
+				}
+
+				Vector2 intercept = GetPlayerRadiusInterceptXZ();
+				Vector3 interceptPos = new Vector3(intercept.X, player.Position.Y, intercept.Y);
+				if (Position.DistanceTo(interceptPos) < standStillDistance)
+				{
+					state = State.StandAndShoot;
+					break;
+				}
+
+				shootComp.TryShoot();
+				movePos = new Vector3(intercept.X, player.Position.Y, intercept.Y);
+				break;
+			}
+			case State.StandAndShoot:
+			{
+				Vector2 intercept = GetPlayerRadiusInterceptXZ();
+				Vector3 interceptPos = new Vector3(intercept.X, player.Position.Y, intercept.Y);
+				if (Position.DistanceTo(interceptPos) > standStillDistance)
+				{
+					state = State.MoveAndShoot;
+				}
+				shootComp.TryShoot();
+				break;
+			}
+			case State.Dead:
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+
+	private Vector2 GetPlayerRadiusInterceptXZ()
+	{
+		Vector2 playerCenter = new Vector2(player.Position.X, player.Position.Z);
+		Vector2 myCenter = new Vector2(Position.X, Position.Y);
+		Vector2 intercept = playerCenter + (myCenter - playerCenter).Normalized() * idealDistanceToPlayer;
+		return intercept;
+	}
+	
+	private void AgentVelocityComputed(Vector3 safe_velocity)
+	{
+		Velocity = Velocity.MoveToward(safe_velocity, 0.25f);
+		MoveAndSlide();
+	}
+	
+	private enum State
+	{
+		Idle,
+		MoveToPlayer,
+		MoveAndShoot,
+		StandAndShoot,
+		Stunned,
+		Dead
+	}
+}
+
+
+
