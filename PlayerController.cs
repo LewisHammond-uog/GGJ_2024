963ae5c962e84c1223e5ccba515f7015f4cdb4da
HungryHorse <jackbrewer@hungrydevstudios.com>
2024-01-27 12:16:00 +0000

    Basic Movement

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,12 +6,35 @@ public partial class PlayerController : CharacterBody3D
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
 
+	[Export] public float Sensitivity = 1.0f;
+
+	public Vector2 lookDirection;
+	[Export] public Camera3D camera;
+
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion motion)
+		{
+			lookDirection = motion.Relative * 0.001f;
+			if(Input.MouseMode == Input.MouseModeEnum.Captured)
+				Look();
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
+
+		if (Input.IsActionPressed("escape_action"))
+			Input.MouseMode = Input.MouseModeEnum.Visible;
 
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -21,9 +44,9 @@ public partial class PlayerController : CharacterBody3D
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
+#region Movement
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("movement_left", "movement_right", "movement_up", "movement_down");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
@@ -38,5 +61,17 @@ public partial class PlayerController : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+#endregion
+	}
+
+	private void Look()
+	{
+		Vector3 lookVector = camera.Rotation;
+		lookVector.X = Math.Clamp(lookVector.X - (lookDirection.Y * Sensitivity), -1.5f,1.5f);
+		camera.Rotation = lookVector;
+
+		Vector3 playerRotation = Rotation;
+		playerRotation.Y -= lookDirection.X * Sensitivity;
+		Rotation = playerRotation;
 	}
 }
