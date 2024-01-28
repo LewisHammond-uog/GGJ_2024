ce0ac5889857d3b739e5deca23999953d5405062
Doglodyte <benwelljoshua@gmail.com>
2024-01-28 14:05:15 +0000

    added win condition

---
+++ Scripts/helpme.gd
@@ -0,0 +1,18 @@
+extends Label3D
+
+var option : String
+var count : int
+@export var dialogue : Array[String]
+
+func _ready():
+	count = 0
+	option = dialogue[count]
+	text = option
+
+func _on_timer_timeout():
+	if option < dialogue.max():
+		count += 1
+	else:
+		count = 0
+	option = dialogue[count]
+	text = option
